aeee47601da19671f6677c156b69a61a9cc67db7
AmyFong82 <amyhfong@gmail.com>
2021-03-17 00:33:05 -0400

    added migrations and seeds for testing

---
+++ sight-words-games-backend/db/schema.rb
@@ -0,0 +1,45 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2021_03_17_035428) do
+
+  create_table "completed_words", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "sight_word_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["sight_word_id"], name: "index_completed_words_on_sight_word_id"
+    t.index ["user_id"], name: "index_completed_words_on_user_id"
+  end
+
+  create_table "sight_words", force: :cascade do |t|
+    t.string "spelling"
+    t.integer "word_length"
+    t.string "pronunciation_audio"
+    t.string "pronunciation_key"
+    t.string "sentence"
+    t.string "picture"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.integer "completion_status", default: 0
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "completed_words", "sight_words"
+  add_foreign_key "completed_words", "users"
+end
--- sight-words-games-backend/db/seeds.rb
+++ sight-words-games-backend/db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user1 = User.create(name: "User1", email: "user1@email.com")
+
+
+a = SightWord.create(spelling: "a")
+
+completed_words1 = CompletedWord.create(user: user1, sight_word: a)
