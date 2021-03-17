0193c30f95af6e099a34c2655f328b1641965a0f
AmyFong82 <amyhfong@gmail.com>
2021-03-17 00:47:13 -0400

    added more details to the word 'a' in the seed file.

--- sight-words-games-backend/db/seeds.rb
+++ sight-words-games-backend/db/seeds.rb
@@ -9,6 +9,7 @@
 user1 = User.create(name: "User1", email: "user1@email.com")
 
 
-a = SightWord.create(spelling: "a")
+a = SightWord.create(spelling: "a", pronunciation_audio: "https://static.sfdict.com/audio/lunawav/A00/A0000100.ogg", 
+	pronunciation_key: "[ uh; when stressed ey ]", sentence: "May I have a pink popsicle, please?", picture: "https://images.vexels.com/media/users/3/210453/isolated/preview/193ca85105f26dd0dbc539de1c1baf52-pink-popsicle-illustration-by-vexels.png")
 
 completed_words1 = CompletedWord.create(user: user1, sight_word: a)
