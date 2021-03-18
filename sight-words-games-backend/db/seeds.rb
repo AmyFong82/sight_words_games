b3dce034a03b75a14d62dc63aa83c663cf6c3acc
AmyFong82 <amyhfong@gmail.com>
2021-03-17 23:25:29 -0400

    added 10 sight words to the seed file.

--- sight-words-games-backend/db/seeds.rb
+++ sight-words-games-backend/db/seeds.rb
@@ -9,7 +9,70 @@
 user1 = User.create(name: "User1", email: "user1@email.com")
 
 
-a = SightWord.create(spelling: "a", pronunciation_audio: "https://static.sfdict.com/audio/lunawav/A00/A0000100.ogg", 
-	pronunciation_key: "[ uh; when stressed ey ]", sentence: "May I have a pink popsicle, please?", picture: "https://images.vexels.com/media/users/3/210453/isolated/preview/193ca85105f26dd0dbc539de1c1baf52-pink-popsicle-illustration-by-vexels.png")
+a = SightWord.create(spelling: "a", 
+					pronunciation_audio: "https://static.sfdict.com/audio/lunawav/A00/A0000100.ogg", 
+					pronunciation_key: "[ uh; when stressed ey ]", 
+					sentence: "May I have a pink popsicle, please?", 
+					picture: "https://images.vexels.com/media/users/3/210453/isolated/preview/193ca85105f26dd0dbc539de1c1baf52-pink-popsicle-illustration-by-vexels.png")
+
+the = SightWord.create(spelling: "the"
+						pronunciation_audio:
+						pronunciation_key:
+						sentence:
+						picture:)
+
+of = SightWord.create(spelling: "of"
+						pronunciation_audio:
+						pronunciation_key:
+						sentence:
+						picture:)
+
+to = SightWord.create(spelling: "to"
+						pronunciation_audio:
+						pronunciation_key:
+						sentence:
+						picture:)
+
+is = SightWord.create(spelling: "is"
+						pronunciation_audio:
+						pronunciation_key:
+						sentence:
+						picture:)
+
+you = SightWord.create(spelling: "you"
+						pronunciation_audio:
+						pronunciation_key:
+						sentence:
+						picture:)
+
+that = SightWord.create(spelling: "that"
+						pronunciation_audio:
+						pronunciation_key:
+						sentence:
+						picture:)
+
+it = SightWord.create(spelling: "it"
+						pronunciation_audio:
+						pronunciation_key:
+						sentence:
+						picture:)
+
+he = SightWord.create(spelling: "he"
+						pronunciation_audio:
+						pronunciation_key:
+						sentence:
+						picture:)
+
+she = SightWord.create(spelling: "she"
+						pronunciation_audio:
+						pronunciation_key:
+						sentence:
+						picture:)
+
+was = SightWord.create(spelling: "was"
+						pronunciation_audio:
+						pronunciation_key:
+						sentence:
+						picture:)
 
 completed_words1 = CompletedWord.create(user: user1, sight_word: a)
