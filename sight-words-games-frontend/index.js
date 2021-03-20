6363dfa6c2c9dd133db2b8e017f3c9cc6a9e359b
AmyFong82 <amyhfong@gmail.com>
2021-03-20 02:06:19 -0400

    set the word intro section

--- sight-words-games-frontend/index.js
+++ sight-words-games-frontend/index.js
@@ -14,22 +14,47 @@ function renderSightWords(){
 	.then(resp => resp.json())
 	.then(sight_words => {
 		for (const word of sight_words){
-			const a = document.createElement("a")
-			a.classList.add("list-group-item", "list-group-item-action")
-			a.setAttribute("id", `${word.spelling}`)
-			a.innerHTML = word.spelling
-			a.href = "#" 
-			a.addEventListener("click", (e) => {
-				const sight_word = e.currentTarget.getAttribute("id")
-				const game_page = document.querySelector(".exercises_col")
-				const intro_line = document.querySelector("#intro_line")
-				intro_line.style.display = "none";
+			const btn = document.createElement("button")
+			btn.classList.add("list-group-item", "list-group-item-action")
+			btn.setAttribute("id", `${word.spelling}`)
+			btn.innerHTML = word.spelling
+			btn.addEventListener("click", (e) => {
+				fetch(BASE_URL + '/sight_words/' + word.id)
+				.then(resp => resp.json())
+				.then(sight_word => {
+					const game_page = document.querySelector(".exercises_col")
+					const intro_line = document.querySelector("#intro_line")
+					intro_line.style.display = "none";
+					const word_intro = document.querySelector("#word_intro")
+					word_intro.style.display = "block";
+					const h2 = document.querySelector("h2")
+					h2.innerHTML = sight_word.spelling
+					const audio_key = document.querySelector("#pronunciation_key")
+					audio_key.innerHTML = sight_word.pronunciation_key
+					const speaker = document.querySelector("#speaker")
+			        const audio = document.querySelector("audio")
+			        audio.setAttribute("src", `${sight_word.pronunciation_audio}`)
+					speaker.onclick = e => {
+				        e.preventDefault();
+				        audio.play();
+	      			};
+				})
 			})
-			div.append(a)
+			div.append(btn)
 		}
 	})
 }
 
+            // <div>
+            //   <h2 id="main_word">a <span><i color = #6ddccf class="fas fa-volume-up"></i></span></h2>
+            //   <h4 ></h4>
+            // </div>
+
 function startWordGame(){
 	const sight_word = document.querySelector("")
+}
+
+function playAudio(){
+	const audio = document.querySelector("audio")
+	audio.play();
 }
