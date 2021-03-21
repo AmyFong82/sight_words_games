62c820b39d81e7f3e139d7178af65480b2b1d903
AmyFong82 <amyhfong@gmail.com>
2021-03-21 15:01:46 -0400

    extracting fetchSightWord function

--- sight-words-games-frontend/index.js
+++ sight-words-games-frontend/index.js
@@ -3,6 +3,11 @@ const BASE_URL = "http://localhost:3000"
 document.addEventListener("DOMContentLoaded", () => {
 	renderSightWords();
 
+	const startBtn = document.querySelector("#start-arrow")
+	// startBtn.onclick = e => {
+
+	// }
+
 })
 
 function renderSightWords(){
@@ -18,41 +23,63 @@ function renderSightWords(){
 			btn.classList.add("list-group-item", "list-group-item-action")
 			btn.setAttribute("id", `${word.spelling}`)
 			btn.innerHTML = word.spelling
-			btn.addEventListener("click", (e) => {
-				fetch(BASE_URL + '/sight_words/' + word.id)
-				.then(resp => resp.json())
-				.then(sight_word => {
-					const game_page = document.querySelector(".exercises_col")
-					const intro_line = document.querySelector("#intro_line")
-					intro_line.style.display = "none";
-					const word_intro = document.querySelector("#word_intro")
-					word_intro.style.display = "block";
-					const h2 = document.querySelector("h2")
-					h2.innerHTML = sight_word.spelling
-					const audio_key = document.querySelector("#pronunciation_key")
-					audio_key.innerHTML = sight_word.pronunciation_key
-					const speaker = document.querySelector("#speaker")
-			        const audio = document.querySelector("audio")
-			        audio.setAttribute("src", `${sight_word.pronunciation_audio}`)
-					speaker.onclick = e => {
-				        e.preventDefault();
-				        audio.play();
-	      			};
-				})
-			})
+			btn.addEventListener("click", fetchSightWord(word.id))
+			// (e) => {
+			// 	fetch(BASE_URL + '/sight_words/' + word.id)
+			// 	.then(resp => resp.json())
+			// 	.then(sight_word => {
+			// 		hideIntroLine();
+			// 		const word_intro = document.querySelector("#word_intro")
+			// 		word_intro.style.display = "block";
+			// 		const h2 = document.querySelector("h2")
+			// 		h2.innerHTML = sight_word.spelling
+			// 		const audio_key = document.querySelector("#pronunciation_key")
+			// 		audio_key.innerHTML = sight_word.pronunciation_key
+			// 		const speaker = document.querySelector("#speaker")
+			//         const audio = document.querySelector("audio")
+			//         audio.setAttribute("src", `${sight_word.pronunciation_audio}`)
+			//         audio.play();
+			// 		speaker.onclick = e => {
+			// 	        e.preventDefault();
+			// 	        audio.play();
+	  //     			};
+			// 	})
+			// })
 			div.append(btn)
 		}
 	})
 }
 
-            // <div>
-            //   <h2 id="main_word">a <span><i color = #6ddccf class="fas fa-volume-up"></i></span></h2>
-            //   <h4 ></h4>
-            // </div>
-
-function startWordGame(){
-	const sight_word = document.querySelector("")
+function hideIntroLine(){
+	const intro_line = document.querySelector("#intro_line")
+	if (intro_line.style.display = "block") {
+		intro_line.style.display = "none";
+	}
+	
 }
+
+function fetchSightWord(word_id){
+	fetch(BASE_URL + '/sight_words/' + word_id)
+	.then(resp => resp.json())
+	.then(sight_word => {
+		hideIntroLine();
+		const word_intro = document.querySelector("#word_intro")
+		word_intro.style.display = "block";
+		const h2 = document.querySelector("h2")
+		h2.innerHTML = sight_word.spelling
+		const audio_key = document.querySelector("#pronunciation_key")
+		audio_key.innerHTML = sight_word.pronunciation_key
+		const speaker = document.querySelector("#speaker")
+        const audio = document.querySelector("audio")
+        audio.setAttribute("src", `${sight_word.pronunciation_audio}`)
+        audio.play();
+		speaker.onclick = e => {
+	        e.preventDefault();
+	        audio.play();
+		}
+	})
+}
+
 
 function playAudio(){
 	const audio = document.querySelector("audio")
